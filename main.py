237f7e095306f5d87258028ca9c7460e2845abc0
Lee Fely <83705537+LeeFely@users.noreply.github.com>
2023-06-04 18:12:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-# 读取本地PDF
-import 
+＃ read a PDF file in batches
+import PyPDF2
+
+def read_pdf_in_batches(file_path, start_page, end_page):
+    # Open the PDF file in read binary mode
+    with open(file_path, 'rb') as file:
+        # Create a PDF reader object
+        pdf_reader = PyPDF2.PdfFileReader(file)
+
+        # Get the total number of pages in the PDF
+        num_pages = pdf_reader.numPages
+
+        # Validate the start and end page numbers
+        start_page = max(0, min(start_page, num_pages - 1))
+        end_page = min(num_pages, max(end_page, start_page + 1))
+
+        # Read the content of each page in the specified range
+        for page_number in range(start_page, end_page):
+            # Get the specific page
+            page = pdf_reader.getPage(page_number)
+
+            # Extract the text from the page
+            text = page.extract_text()
+
+            # Do something with the extracted text
+            print(f"Page {page_number + 1}:")
+            print(text)
+            print()
+
+# Specify the range of pages you want to read
+start_page = 0
+end_page = 5
+
+# Call the function to read the PDF file in batches
+read_pdf_in_batches('path/to/your/file.pdf', start_page, end_page)
+
